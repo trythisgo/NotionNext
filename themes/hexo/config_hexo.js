cc3a4b66c44081233e8f9ad34a1f422eaff3f3d9
trythisgo <103901809+trythisgo@users.noreply.github.com>
2023-03-31 21:26:18 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,5 +1,5 @@
 const CONFIG_HEXO = {
-  HOME_BANNER_ENABLE: false,
+  HOME_BANNER_ENABLE: true,
   HOME_BANNER_GREETINGS: ['听说', '今晚', '月色很美', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
